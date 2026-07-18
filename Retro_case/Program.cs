430ad2d0a3580c48956e4a124e18cf7066275ae3
J.Norton (Mr James Norton) <norton.j@millfieldschool.com>
2026-07-18 22:28:43 +0100

    pust to fix sid player for music

--- Retro_case/Program.cs
+++ Retro_case/Program.cs
@@ -12,5 +12,6 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 builder.Services.AddSingleton<Retro_case.Services.ThemeState>();
+builder.Services.AddSingleton<Retro_case.Services.AudioSettings>();
 
 await builder.Build().RunAsync();
